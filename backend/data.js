98b2a632c46a9fee9131809823e2c2266042e551
zekiyecakiral <zekiyecakiral@gmail.com>
2020-08-14 13:39:45 +0200

    amazona zc

---
+++ backend/data.js
@@ -0,0 +1,48 @@
+export default  {
+    products : [
+        {
+        _id : '1',
+        name : 'Slim Shirt',
+        category :'Shirt',
+        image : '/images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10,
+        countInStock : 6
+    },
+    {   
+        _id : '2',
+        name : 'Fit Shirt',
+        category :'Shirt',
+        image : '/images/d1.jpg',
+        price : 50,
+        brand : 'Nike',
+        rating : 4.2,
+        numReviews : 8,
+        countInStock : 6
+    },
+    {
+        _id : '3',
+        name : 'Slim Shirt',
+        category: 'Pants',
+        image : '/images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10,
+        countInStock : 6
+    },
+    {
+        _id : '4',
+        name : 'Slim Shirt',
+        category: 'Pants',
+        image : '/images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10,
+        countInStock : 6
+    }
+]
+}
